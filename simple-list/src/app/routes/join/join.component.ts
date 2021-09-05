cb04eeef5eee19abcf8395ed1f6afd86acc67a50
enrique <enrique.rafaelxd@gmail.com>
2021-09-05 08:43:53 -0430

    design improved; home fixed and join

--- simple-list/src/app/routes/join/join.component.ts
+++ simple-list/src/app/routes/join/join.component.ts
@@ -1,7 +1,10 @@
-import { Component, OnInit } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms'
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { FormControl, FormGroup, Validators } from '@angular/forms'
 import {AuthService} from '../../shared/services/auth.service'
 import { Router } from '@angular/router'
+import { comparePasswords } from './join.validator'
+import { Subscription } from 'rxjs'
+
 @Component({
   selector: 'app-join',
   templateUrl:'join.component.html',
@@ -18,13 +21,37 @@ export class JoinComponent implements OnInit {
   }
 
   join = new FormGroup({
-  	username : new FormControl(''),
-  	email: new FormControl(''),
-  	password: new FormControl('')
+  	username : new FormControl('', [Validators.required, Validators.minLength(6)]),
+  	email: new FormControl('', [Validators.email, Validators.required]),
+  	password: new FormControl('', [Validators.required, Validators.minLength(6) ]),
+    repeatedPassword: new FormControl('',[Validators.required, Validators.minLength(6)])
+  },{
+    validators: comparePasswords
   })
+
+  checkPasswords(): boolean {
+
+    const passwordInput = this.join.get('password')
+    const repeatedPasswordInput = this.join.get('password')
+    
+    return this.join.hasError('notEqual') && passwordInput.dirty && repeatedPasswordInput.dirty
+  }
+
+  checkUsername():boolean {
+
+    const usernameInput = this.join.get('username')
+    return usernameInput.hasError('minlength') && usernameInput.dirty
+  }
+
+  checkEmail():boolean {
+
+    const emailInput = this.join.get('email')
+    return emailInput.hasError('email') && emailInput.dirty
+  }
 
   onJoin():void{
     const data = this.join.value
+    delete data.repeatedPassword
 
     this.authSvc.join(data).subscribe((res) => {
       if(res){
